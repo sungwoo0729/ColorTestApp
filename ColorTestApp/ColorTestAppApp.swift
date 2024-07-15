481bb5a08e621a37f1a05dc235230861d08eda5c
com.Daniel <cjstjddn20@gmail.com>
2024-07-16 04:41:40 +0900

    Create Xcode Project

---
+++ ColorTestApp/ColorTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTestAppApp.swift
+//  ColorTestApp
+//
+//  Created by sungwoo0729 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
